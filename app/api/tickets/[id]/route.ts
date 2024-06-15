5dd0024015d271fc6fca8f8fe765be344320dcb4
Shayne Meyer <shayne.meyer@gmail.com>
2024-06-15 07:15:00 -0700

    added delete ticket endpoint

--- app/api/tickets/[id]/route.ts
+++ app/api/tickets/[id]/route.ts
@@ -29,3 +29,19 @@ export async function PATCH(request: NextRequest, { params }: Props) {
 
   return NextResponse.json(updatedTicket, { status: 200 });
 }
+
+export async function DELETE(request: NextRequest, { params }: Props) {
+  const ticket = await prisma.ticket.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!ticket) {
+    return NextResponse.json("Ticket not found", { status: 404 });
+  }
+
+  await prisma.ticket.delete({
+    where: { id: ticket.id },
+  });
+
+  return NextResponse.json("Ticket deleted", { status: 200 });
+}
